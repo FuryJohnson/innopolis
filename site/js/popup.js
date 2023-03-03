6e0ae9b0f56c4a08b3ebcfa694368bccd39532f2
Igor Borzunov <igorborzunov@proton.me>
2023-03-03 18:49:12 +0300

    commit

--- site/js/popup.js
+++ site/js/popup.js
@@ -1,7 +1,9 @@
 let loginButton = document.querySelector('.login-button');
 let loginPopUp = document.querySelector('.login-popup');
+let closeButton = document.querySelector('.popup-close');
 
 loginButton.addEventListener('click', showPopup);
+closeButton.addEventListener('click', close);
 
 function showPopup(evt) {
     console.log(evt);
@@ -9,3 +11,13 @@ function showPopup(evt) {
 
     loginPopUp.classList.toggle('show-popup');
 };
+
+function close() {
+    loginPopUp.classList.remove('show-popup')
+}
+
+loginPopUp.addEventListener('click', (e) => {
+    if (e.target.classList.contains('show-popup')) {
+        close();
+    }
+})
